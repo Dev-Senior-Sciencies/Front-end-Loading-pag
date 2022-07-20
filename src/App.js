41af7d6bc59ba63e8f3a104441b70d9729f021ec
Samuel Lucas <samuellucas24383@gmail.com>
2022-07-20 07:42:51 -0300

    :bulb: refactor: My code new Pull request

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Header from './components/header';
+/* import Anuncios from './components/anuncios';
+import Servicos from './components/servicos';
+import Indicadores from './components/indicadores';
+import Footer from './components/footer'; */
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+{/*       <Anuncios />
+      <Servicos />
+      <Indicadores />
+      <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
