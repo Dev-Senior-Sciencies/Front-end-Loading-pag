f9876f6ac3b4a577ca6302069b5a091c4e28eb8e
Samuel Lucas <samuellucas24383@gmail.com>
2022-07-22 20:17:42 -0300

    :sparkles: Refactor: My code

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,50 +1,124 @@
-import logoNovaSc from '../images/logo_nova_sc.png';
-import hamburguer from '../icons/hamburguer.png';
-import facebook from '../icons/facebook.png';
-import twitter from '../icons/twitter.png';
-import youtube from '../icons/youtube.png';
-import instagram from '../icons/instagram.png';
-import flickr from '../icons/flickr.png';
-import tiktok from '../icons/tiktok.png';
-import opcao from '../icons/opcao.png';
+import logoNovaSc from "../images/logo_nova_sc.png";
+import Hamburger from "../icons/hamburguer.png";
+import facebook from "../icons/facebook.png";
+import twitter from "../icons/twitter.png";
+import youtube from "../icons/youtube.png";
+import instagram from "../icons/instagram.png";
+import flickr from "../icons/flickr.png";
+import tiktok from "../icons/tiktok.png";
+import opcao from "../icons/opcao.png";
 
-import './header.css';
+import "./header.css";
 
 function Header() {
   return (
-      <header>
-        <nav className="container-links">
-          <div className='container-link-rede-social'>
-            <img className='logoNovaSc' src={logoNovaSc} alt="" />
-            <div className='links-nav'>
-              <a href="#.">NOTÍCIAS</a>
-              <a href="#.">SERVIÇOS</a>
-              <a href="#.">DIÁRIO OFICIAL</a>
-              <a href="#.">OUVIDORIA</a>
-              <a href="#.">TRNSPARÊNCIA</a>
-              <a href="#.">ORGÃOS DO GOVERNO</a>
-            </div>
-            <div className='container-i-img'>
-              <i><img src={facebook} alt="facebook icon" /></i>
-              <i><img src={twitter} alt="twitter icon" /></i>
-              <i><img src={youtube} alt="youtube icon" /></i>
-              <i><img src={instagram} alt="instagram icon" /></i>
-              <i><img src={flickr} alt="flickr icon" /></i>
-              <i><img src={tiktok} alt="tiktok icon" /></i>
-            </div>
-          </div>
-          <div className='container-link-rede-buttons'>
-            <a className='container-link-Portal' href="#.">
-              <i><img className='container-link-hamburger' src={hamburguer} alt=""/></i>
-              <p className='container-link-p'>Portal de Serviços</p>
+    <header className="flex justify-center h-full max-h-40 w-screen">
+      <nav className="flex flex-col max-h-48 max-w-5xl h-full w-full">
+        <div className="flex flex-row justify-center items-center self-center max-w-5xl w-full h-3/5 ">
+          <img className="flex h-16 w-60" src={logoNovaSc} alt="" />
+          <div className="flex justify-center h-full w-11/12 max-h-10 relative ln no-underline">
+            <a
+              className="text-xs font-light relative crm hover:text-red-700"
+              href="#."
+            >
+              NOTÍCIAS
             </a>
-            <a className='container-link-entrar' href="#.">
-              <i><img src={opcao} alt="opcao icon"/></i>
-              <p className='container-link-E'>ENTRAR</p>
+            <a
+              className="text-xs font-light relative crm hover:text-red-700"
+              href="#."
+            >
+              SERVIÇOS
+            </a>
+            <a
+              className="text-xs font-light relative crm hover:text-red-700"
+              href="#."
+            >
+              DIÁRIO OFICIAL
+            </a>
+            <a
+              className="text-xs font-light relative crm hover:text-red-700"
+              href="#."
+            >
+              OUVIDORIA
+            </a>
+            <a
+              className="text-xs font-light relative crm hover:text-red-700"
+              href="#."
+            >
+              TRNSPARÊNCIA
+            </a>
+            <a
+              className="text-xs font-light relative crm hover:text-red-700"
+              href="#."
+            >
+              ORGÃOS DO GOVERNO
             </a>
           </div>
-        </nav>
-      </header>
+          <div className="flex justify-center items-center relative bottom-3 max-h-6 h-full w-48 right-20">
+            <i>
+              <img
+                className="max-h-3 h-full mr-4"
+                src={facebook}
+                alt="facebook icon"
+              />
+            </i>
+            <i>
+              <img
+                className="max-h-3 h-full mr-4"
+                src={twitter}
+                alt="twitter icon"
+              />
+            </i>
+            <i>
+              <img
+                className="max-h-3 h-full mr-4"
+                src={youtube}
+                alt="youtube icon"
+              />
+            </i>
+            <i>
+              <img
+                className="max-h-3 h-full mr-4"
+                src={instagram}
+                alt="instagram icon"
+              />
+            </i>
+            <i>
+              <img className="max-h-3 h-full" src={flickr} alt="flickr icon" />
+            </i>
+            <i>
+              <img className="max-h-3 h-full" src={tiktok} alt="tiktok icon" />
+            </i>
+          </div>
+        </div>
+        <div className="flex flex-row w-full justify-between max-w-screen-lg">
+          <a className="flex flex-row w-full h-full left-6" href="#.">
+            <i>
+              <img
+                className="h-10r relative top-0.5"
+                src={Hamburger}
+                alt="Menu do Hamburger icon"
+              />
+            </i>
+            <p className="h-full font-normal ml-2 text-3xl cP">
+              Portal de Serviços
+            </p>
+          </a>
+          <a className="flex flex-row" href="#.">
+            <i>
+              <img
+                className="h-3 mr-4 relative right-16 top-3.5"
+                src={opcao}
+                alt="opcao icon"
+              />
+            </i>
+            <p className="h-8 mr-2 relative text-sm right-16 top-1/4 colorP">
+              ENTRAR
+            </p>
+          </a>
+        </div>
+      </nav>
+    </header>
   );
 }
 
